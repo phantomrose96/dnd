6460a02a771d2b0957f8b75cf4ca2dcb3e3fdbf9
Chrissy Long <chrissyrlong@gmail.com>
2019-12-19 20:08:17 -0500

    hide tokeen

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const fs = require('fs');
 const bootUp = require('./botInstance');
-let MAP = null;
+const MAP = require('./token.json')
+const characters = MAP.characters;
+for( let key of Object.keys(characters)) {
+    bootUp(characters[key].token);
+};
 
-fs.readFile('./loadfile.json', (err, map) => {
-    if(err) {
-        console.log(err);
-    } else {
-        MAP = JSON.parse(map);
-    }
-});
-
-setTimeout(() => {
-    const characters = MAP["characters"];
-    for( let key of Object.keys(characters)) {
-        bootUp(characters[key]["token"]);
-    };
-}, 200);
 
